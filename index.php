cc9302be685df061da2e2e2619fded8c6a4aa2c0
Simon Turquier <turquiersimon@hotmail.fr>
2018-03-29 09:56:25 +0200

    Add AWS dependency

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 include 'app/vendor/autoload.php';
 
-$test = new App\Test();
+// $test = new App\Test();
+
+$s3 = new Aws\S3\S3Client([
+	'version'	=> 'latest',
+	'region'	=> 'eu-west-1'
+]);
+
+var_dump($s3);
